3994594a1e87eeaf2df81ec24e67cc337a84824d
rainyjune@live.cn <rainyjune@live.cn@b4eaa4ea-357a-11de-8b57-c5b9ca66747e>
2009-05-19 08:14:38 +0000

    更新site.conf.php权限为0646

--- trunk/adm/site.conf.php
+++ trunk/adm/site.conf.php
@@ -2,9 +2,9 @@
 	 $board_name='枫叶留言板';
 	 $admin_email='rainyjune@live.cn';
 	 $copyright_info='Copyright © 2009 maple.dreamneverfall.cn';
-	 $filter_words='fuck,TMD,西藏,台湾';
+	 $filter_words='fuck,TMD,西藏,台湾,北京';
 	 $valid_code_open=0;
 	 $page_on=0;
 	 $num_perpage=1;
-	 $theme='simple';
+	 $theme='default';
 ?>
