d3dbc96bb91e7004bf7a8914f031f8db04337f46
dreamneverfall <dreamneverfall@b4eaa4ea-357a-11de-8b57-c5b9ca66747e>
2010-04-28 10:27:11 +0000

    删除模板文件 reply_update.php，不再需要。

--- trunk/themes/simple/templates/reply_update.php
+++
@@ -1,6 +0,0 @@
-<form action="index.php?action=reply_update" method="post">
-<input type="hidden" name="mid" value="<?php echo $mid;?>" />
-<textarea name="reply_content" cols="40" rows="9"><?php echo $reply_data[1];?></textarea>
-<br />
-<input type="submit" name="Submit" value="提交" /><input type="button" name="cancel" value="取消" onclick="javascript:window.open('index.php?action=control_panel&subtab=message','_self')" />
-</form>
