999281735bc0359b8a58dd205540858ca0c75138
dreamneverfall <dreamneverfall@b4eaa4ea-357a-11de-8b57-c5b9ca66747e>
2010-10-19 02:31:24 +0000

    在源码中删除了 jquery ，改为使用 googleapi 提供的在线 jquery.

--- themes/simple/templates/admin.php
+++ themes/simple/templates/admin.php
@@ -7,7 +7,7 @@
 <meta http-equiv="expires" content="0" />
 <link type="text/css" rel="stylesheet" href="<?php echo './themes/'.$this->_theme.'/admin.css';?>" />
 <script type="text/javascript" src="./includes/admin.js"></script>
-<script type="text/javascript" src="./includes/jquery.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo './themes/'.$this->_theme.'/admin.js';?>"></script>
 <title><?php echo $this->t('ACP_INDEX');?></title>
 </head>
