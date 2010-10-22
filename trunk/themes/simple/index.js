0b21499cb5ad7200eddde0c8e98944fe2af2ce89
dreamneverfall <dreamneverfall@b4eaa4ea-357a-11de-8b57-c5b9ca66747e>
2010-10-22 09:26:46 +0000

    使用 jQuery 实现 enter + ctrl 提交。

--- trunk/themes/simple/index.js
+++ trunk/themes/simple/index.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
+	$(document).keypress(function(e){
+		if(e.ctrlKey && e.which == 13 || e.which == 10) {
+			$("#guestbook").submit();
+		} else if (e.shiftKey && e.which==13 || e.which == 10) {
+			$("#guestbook").submit();
+		}
+	});
 	$('#guestbook').submit(function(){
 		checkall();
 		$.ajax({
-		   type: "POST",
-		   url: "index.php?action=post",
-		   data: $(this).serialize(),
-		   success: function(){
-			//$(this).replaceWith($(this));
-			 $.get('index.php?action=ajaxIndex',{ajax:'yes'},function(data){
-				//alert(data);
+			type: "POST",
+			url: "index.php?action=post",
+			data: $(this).serialize(),
+			success: function(){
+			document.getElementById('guestbook').reset();
+			$.get('index.php?action=ajaxIndex',{ajax:'yes'},function(data){
+			//alert(data);
 				$("tr").remove(".message");
 				$(".header").after(data);
 			 });
@@ -67,55 +74,3 @@ function insert_smiley()
 	document.getElementById("content").value +=" " + this.id;
 	return false;
 }
-// function isKeyTrigger(e,keyCode){
-    // var argv = isKeyTrigger.arguments;
-    // var argc = isKeyTrigger.arguments.length;
-    // var bCtrl = false;
-    // if(argc > 2){
-        // bCtrl = argv[2];
-    // }
-    // var bAlt = false;
-    // if(argc > 3){
-        // bAlt = argv[3];
-    // }
-
-    // var nav4 = window.Event ? true : false;
-
-    // if(typeof e == 'undefined') {
-        // e = event;
-    // }
-
-    // if( bCtrl && 
-        // !((typeof e.ctrlKey != 'undefined') ? 
-            // e.ctrlKey : e.modifiers & Event.CONTROL_MASK > 0)){
-        // return false;
-    // }
-    // if( bAlt && 
-        // !((typeof e.altKey != 'undefined') ? 
-            // e.altKey : e.modifiers & Event.ALT_MASK > 0)){
-        // return false;
-    // }
-    // var whichCode = 0;
-    // if (nav4) whichCode = e.which;
-    // else if (e.type == "keypress" || e.type == "keydown")
-        // whichCode = e.keyCode;
-    // else whichCode = e.button;
-
-    // return (whichCode == keyCode);
-// }
-
-// function ctrlEnter(e){
-    // var ie =navigator.appName=="Microsoft Internet Explorer"?true:false; 
-    // if(ie){
-        // if(event.ctrlKey && window.event.keyCode==13){doSomething();}
-    // }else{
-        // if(isKeyTrigger(e,13,true)){doSomething();}
-    // }
-// }
-// function doSomething()
-// {
-		// if(checkall())
-		// {
-			// document.guestbook.submit.click();
-		// }
-// } 
