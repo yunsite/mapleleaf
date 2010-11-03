d7c08564eaf331ddee286c325c27210428e8c5e9
dreamneverfall <dreamneverfall@b4eaa4ea-357a-11de-8b57-c5b9ca66747e>
2010-11-03 06:46:55 +0000

    使用了 JSON

--- trunk/includes/showSmiley.php
+++ trunk/includes/showSmiley.php
@@ -1,56 +1,16 @@
 <?php
-return  <<<EOF
-		<table id="smiley_table" cellpadding="4" cellspacing="0">
-		<tr>
-			<td><a href="#" id=":-)"><img src="{$this->_smileys_dir}grin.gif" alt="grin" title="grin" class="smiley_img" /></a></td>
-			<td><a href="#" id=":lol:"><img src="{$this->_smileys_dir}lol.gif" class="smiley_img" title="LOL" alt="lol"  /></a></td>
-			<td><a href="#" id=':cheese:'><img src="{$this->_smileys_dir}cheese.gif" title="cheese" alt="cheese" class="smiley_img" /></a></td>
-			<td><a href="#" id=':)'><img src="{$this->_smileys_dir}smile.gif" title="smile" alt="smile" class="smiley_img" /></a></td>
-			<td><a href="#" id=';-)'><img src="{$this->_smileys_dir}wink.gif" title="wink" alt="wink" class="smiley_img" /></a></td>
-			<td><a href="#" id=':smirk:'><img src="{$this->_smileys_dir}smirk.gif" title="smirk" alt="smirk" class="smiley_img"/></a></td>
-			<td><a href="#" id=':roll:'><img src="{$this->_smileys_dir}rolleyes.gif" title="rolleyes" alt="rolleyes" class="smiley_img" /></a></td>
-			<td><a href="#" id=':-S'><img src="{$this->_smileys_dir}confused.gif" title="confused" alt="confused" class="smiley_img" /></a></td>
-		</tr>
-		<tr>
-			<td><a href="#" id=":wow:"><img src="{$this->_smileys_dir}surprise.gif" title="surprised" alt="surprised" class="smiley_img" /></a></td>
-			<td><a href="#" id=':bug:'><img src="{$this->_smileys_dir}bigsurprise.gif" title="big surprise" alt="big surprise" class="smiley_img" /></a></td>
-			<td><a href="#" id=':-P'><img src="{$this->_smileys_dir}tongue_laugh.gif" title="tongue laugh" alt="tongue laugh" class="smiley_img" /></a></td>
-			<td><a href="#" id='%-P'><img src="{$this->_smileys_dir}tongue_rolleye.gif" class="smiley_img" title="tongue rolleye" alt="tongue rolleye" /></a></td>
-			<td><a href="#" id=';-P'><img src="{$this->_smileys_dir}tongue_wink.gif" class="smiley_img" title="tongue wink" alt="tongue wink" style="border:0;" /></a></td>
-			<td><a href="#" id=':P'><img src="{$this->_smileys_dir}raspberry.gif" class="smiley_img" title="raspberry" alt="raspberry" style="border:0;" /></a></td>
-			<td><a href="#" id=':blank:'><img src="{$this->_smileys_dir}blank.gif" class="smiley_img" title="blank stare" alt="blank stare" style="border:0;" /></a></td>
-			<td><a href="#" id=':long:'><img src="{$this->_smileys_dir}longface.gif" class="smiley_img" title="long face" alt="long face" style="border:0;" /></a></td>
-		</tr>
-		<tr>
-			<td><a href="#" id=':ohh:'><img src="{$this->_smileys_dir}ohh.gif" class="smiley_img" title="ohh" alt="ohh" style="border:0;" /></a></td>
-			<td><a href="#" id=':grrr:'><img src="{$this->_smileys_dir}grrr.gif" class="smiley_img" title="grrr" alt="grrr"  /></a></td>
-			<td><a href="#" id=':gulp:'><img src="{$this->_smileys_dir}gulp.gif" class="smiley_img" title="gulp" alt="gulp"  /></a></td>
-			<td><a href="#" id='8-/'><img src="{$this->_smileys_dir}ohoh.gif" class="smiley_img" title="oh oh" alt="oh oh"  /></a></td>
-			<td><a href="#" id=':down:'><img src="{$this->_smileys_dir}downer.gif" class="smiley_img" title="downer" alt="downer"  /></a></td>
-			<td><a href="#" id=':red:'><img src="{$this->_smileys_dir}embarrassed.gif" class="smiley_img" title="red face" alt="red face"  /></a></td>
-			<td><a href="#" id=':sick:'><img src="{$this->_smileys_dir}sick.gif" class="smiley_img" title="sick" alt="sick"  /></a></td>
-			<td><a href="#" id=':shut:'><img src="{$this->_smileys_dir}shuteye.gif" class="smiley_img" title="shut eye" alt="shut eye"  /></a></td>
-		</tr>
-		<tr>
-			<td><a href="#" id=':-/'><img src="{$this->_smileys_dir}hmm.gif" class="smiley_img" title="hmmm" alt="hmmm"  /></a></td>
-			<td><a href="#" id='>:('><img src="{$this->_smileys_dir}mad.gif" class="smiley_img" title="mad" alt="mad"  /></a></td>
-			<td><a href="#" id='>:-('><img src="{$this->_smileys_dir}angry.gif" class="smiley_img" title="angry" alt="angry"  /></a></td>
-			<td><a href="#" id=':zip:'><img src="{$this->_smileys_dir}zip.gif" class="smiley_img" title="zipper" alt="zipper"  /></a></td>
-			<td><a href="#" id=':kiss:'><img src="{$this->_smileys_dir}kiss.gif" class="smiley_img" title="kiss" alt="kiss"  /></a></td>
-			<td><a href="#" id=':ahhh:'><img src="{$this->_smileys_dir}shock.gif" class="smiley_img" title="shock" alt="shock"  /></a></td>
-			<td><a href="#" id=':coolsmile:'><img src="{$this->_smileys_dir}shade_smile.gif" class="smiley_img" title="cool smile" alt="cool smile"  /></a></td>
-			<td><a href="#" id=':coolsmirk:'><img src="{$this->_smileys_dir}shade_smirk.gif" class="smiley_img" title="cool smirk" alt="cool smirk"  /></a></td>
-		</tr>
-		<tr>
-			<td><a href="#" id=':coolgrin:'><img src="{$this->_smileys_dir}shade_grin.gif" class="smiley_img" title="cool grin" alt="cool grin"  /></a></td>
-			<td><a href="#" id=':coolhmm:'><img src="{$this->_smileys_dir}shade_hmm.gif" class="smiley_img" title="cool hmm" alt="cool hmm"  /></a></td>
-			<td><a href="#" id=':coolmad:'><img src="{$this->_smileys_dir}shade_mad.gif" class="smiley_img" title="cool mad" alt="cool mad"  /></a></td>
-			<td><a href="#" id=':coolcheese:'><img src="{$this->_smileys_dir}shade_cheese.gif" class="smiley_img" title="cool cheese" alt="cool cheese"  /></a></td>
-			<td><a href="#" id=':vampire:'><img src="{$this->_smileys_dir}vampire.gif" class="smiley_img" title="vampire" alt="vampire"  /></a></td>
-			<td><a href="#" id=':snake:'><img src="{$this->_smileys_dir}snake.gif" class="smiley_img" title="snake" alt="snake"  /></a></td>
-			<td><a href="#" id=':exclaim:'><img src="{$this->_smileys_dir}exclaim.gif" class="smiley_img" title="excaim" alt="excaim"  /></a></td>
-			<td><a href="#" id=':question:'><img src="{$this->_smileys_dir}question.gif" class="smiley_img" title="question" alt="question"  /></a></td>
-		</tr>
-		</table>
-EOF;
+$smileyString="<table id='smileysTable' cellpadding='4'>\n";
+$smileyArray=$this->_smileys;
+$numPerRow=8;
+for($i=0,$c=ceil(count($smileyArray)/$numPerRow);$i<$c;$i++){
+
+    $smileyString.="<tr>\n";
+    $rowArray=array_slice($smileyArray, $i*$numPerRow, $numPerRow);
+    foreach ($rowArray as $key=>$perSmiley){
+        $smileyString.="<td><img id='".$key."' src='".$this->_smileys_dir.$perSmiley[0]."' alt='$perSmiley[3]' title='$perSmiley[3]' /></td>\n";
+    }
+    $smileyString.="</tr>\n";
+}
+$smileyString.="</table>\n";
+return $smileyString;
 ?>
