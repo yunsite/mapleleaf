5934ffd3383c8cc24753b0deea3dd03bd9fe674c
dreamneverfall <dreamneverfall@b4eaa4ea-357a-11de-8b57-c5b9ca66747e>
2010-12-09 05:19:42 +0000

    增加了禁用插件功能(删除插件的配置文件)

--- plugins/fetion.php
+++ plugins/fetion.php
@@ -22,7 +22,10 @@ function fetion_config($showConfig=FALSE,$config=NULL){
     echo "<input type='hidden' name='plugin' value='fetion' />";
     echo '<p>FetionID<input type="text" name="fetionID" value="'.@$fetionID.'" /></p>';
     echo '<p>FetionPassword<input type="password" name="fetionPASSWORD" value="'.@$fetionPWD.'" /></p>';
-    echo '<p><input type="submit" value="submit" /></p>';
+    echo '<p><input type="submit" value="submit" />&nbsp;';
+    if(file_exists($filename))
+        echo '<a href="index.php?controller=plugin&amp;action=deactivate&amp;id=fetion"><input type="button" value="Disable" /></a>';
+    echo '</p>';
     echo '</form>';
 }
 function fetion_send(){
