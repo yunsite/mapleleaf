04983e9957daa601621c01f749fcebe5c60f2501
dreamneverfall <dreamneverfall@b4eaa4ea-357a-11de-8b57-c5b9ca66747e>
2010-12-09 03:51:59 +0000

    为了减少体积，删除了 license.txt 和 readme.html

--- trunk/index.php
+++ trunk/index.php
@@ -1,4 +1,19 @@
 <?php
+/*
+This file is part of MapleLeaf Guestbook.
+
+MapleLeaf Guestbook is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License  version 2 published by
+the Free Software Foundation.
+
+MapleLeaf Guestbook is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html/>.
+*/
 session_start();
 define('IN_MP',true);
 define('APPROOT', dirname(__FILE__));
