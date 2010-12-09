9710fd24a17dd05c5e2dc2e79cca868ecb093d4d
dreamneverfall <dreamneverfall@b4eaa4ea-357a-11de-8b57-c5b9ca66747e>
2010-12-09 05:19:42 +0000

    增加了禁用插件功能(删除插件的配置文件)

--- trunk/includes/controllers/PluginController.php
+++ trunk/includes/controllers/PluginController.php
@@ -11,4 +11,13 @@ class PluginController extends BaseController
         }
         header("Location:index.php?action=control_panel&subtab=plugin");exit;
     }
+    public function actionDeactivate(){
+        is_admin();
+        $all_plugin=ZFramework::get_all_plugins();
+        if(isset ($_GET['id']) && in_array($_GET['id'], $all_plugin)){
+            $pluginConfFile=PLUGINDIR.'.'.$_GET['id'].'.conf.php';
+            unlink($pluginConfFile);
+        }
+        header("Location:index.php?action=control_panel&subtab=plugin");
+    }
 }
