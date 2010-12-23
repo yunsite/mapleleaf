6b1763ee09db8ec709b7f636953d13d9ca8af17d
dreamneverfall <dreamneverfall@b4eaa4ea-357a-11de-8b57-c5b9ca66747e>
2010-12-23 11:32:05 +0000

    Fix issue22

--- themes/simple/scripts/admin.js
+++ themes/simple/scripts/admin.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
+        var containerDiv=new Array('overviewContainer','configContainer','message_container','ip_container','pluginContainer');
 	$.ajax({ type: "GET", url: 'index.php', data: { action: "getSysJSON" }, success: function(data){ languageTips=data;}, dataType: 'json'});
 	/* jqModal */
 	$('#ex2').jqm({ajax: '@href', trigger: 'a.ex2trigger'});
 	$('#deleteallButton').click(function(){ if(window.confirm(languageTips.DEL_ALL_CONFIRM)){window.open('index.php?controller=post&action=deleteAll','_self'); }return false;});
 	$('#deleteallreplyButton').click(function(){if(window.confirm(languageTips.DEL_ALL_REPLY_CONFIRM)){ window.open('index.php?controller=reply&action=deleteAll','_self'); } return false;	});
-	$('#tags li').click(function(){	$('#tags li a').attr('href','javascript:void(0)'); $('#tags li').removeClass('selectTag');  $(this).addClass('selectTag');$('#tagContent div').hide();var indexOfTag=$('#tags li').index(this);$('#tagContent div:eq('+indexOfTag+')').show(); });
-	$("td.admin_message >span").addClass("hidden");
-	$("td.admin_message").hover(function(){	$(this).children("span").removeClass("hidden");	},function(){$(this).children("span").addClass("hidden");});
+	$('#tags li').click(function(){	$('#tags li a').attr('href','javascript:void(0)'); $('#tags li').removeClass('selectTag');  $(this).addClass('selectTag');$('#tagContent div').hide();var indexOfTag=$('#tags li').index(this);$('#'+containerDiv[indexOfTag]).show();$('#'+containerDiv[indexOfTag]+' div').show(); });
+	$("td.admin_message div >span").addClass("hidden");
+	$("td.admin_message div").hover(function(){	$(this).children("span").removeClass("hidden");	},function(){$(this).children("span").addClass("hidden");});
 	$('#m_checkall').click(function(){$("input[name='select_mid[]']").each(function(){$(this).attr('checked',true)});});
 	$('#m_checknone').click(function(){$("input[name='select_mid[]']").each(function(){$(this).attr('checked',false)});});
 	$('#m_checkxor').click(function(){$("input[name='select_mid[]']").each(function(){$(this).attr('checked',!$(this).attr('checked'))});});
