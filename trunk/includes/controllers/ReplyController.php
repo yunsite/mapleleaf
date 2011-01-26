cd0d05ea00138221992a7728418586e96c3ae744
dreamneverfall <dreamneverfall@b4eaa4ea-357a-11de-8b57-c5b9ca66747e>
2011-01-26 01:55:20 +0000

    完善 ReplyController

--- trunk/includes/controllers/ReplyController.php
+++ trunk/includes/controllers/ReplyController.php
@@ -1,37 +1,29 @@
 <?php
-class ReplyController extends BaseController
-{
+class ReplyController extends BaseController{
     public $_model;
-    public function  __construct()
-    {
-        $this->_model=new JuneTxtDB();
-        $this->_model->select_db(DB);
+    public function  __construct(){
+        global $db_url;
+        $this->_model=  YDB::factory($db_url);
     }
     /**
      * create and update
      */
-    public function actionReply()
-    {
+    public function actionReply(){
         is_admin();
 	if($_POST){
 	    $mid=0;
 	    $mid=(int)$_POST['mid'];
-	    $reply_content = ZFramework::maple_quotes($_POST['reply_content']);
+	    $reply_content = ZFramework::maple_quotes($_POST['content']);
 	    $reply_content = nl2br($reply_content);
 	    $reply_content = str_replace(array("\n", "\r\n", "\r"), '', $reply_content);
 	    $time=time();
 	    if (trim($reply_content)=='')
 		ZFramework::show_message(ZFramework::t('REPLY_EMPTY'),true,'index.php?action=control_panel&subtab=message',3);
 	    if(isset($_POST['update'])){
-		$input=array($mid,$reply_content,$time);
-		$condition=array('id'=>$mid);
-		if(!$this->_model->update(REPLYTABLE, $condition, $input))
-		    die($this->_model->error());
+                $this->_model->query("UPDATE reply SET content='$reply_content' WHERE pid=$mid");
 	    }
 	    else{
-		$input=array($mid,$reply_content,$time);
-		if(!$this->_model->insert(REPLYTABLE, $input))
-		    die($this->_model->error());
+                $this->_model->query("INSERT INTO reply ( pid , content , r_time ) VALUES ( $mid , '$reply_content' , $time )");
 	    }
 	    header("Location:index.php?action=control_panel&subtab=message");exit;
 	}
@@ -40,37 +32,30 @@ class ReplyController extends BaseController
 	include 'themes/'.ZFramework::app()->theme.'/templates/'."reply.php";
     }
 
-    protected function loadModel()
-    {
-	if(!isset($_GET['mid']))
-	{
+    protected function loadModel(){
+	if(!isset($_GET['mid'])){
 	    header("location:index.php?action=control_panel&subtab=message");
 	    exit;
 	}
 	$mid=(int)$_GET['mid'];
-	$condition=array('id'=>$mid);
-	$reply_data=$this->_model->select(REPLYTABLE, $condition);
-	if ($reply_data===FALSE)
-	    ZFramework::show_message(ZFramework::t("QUERY_ERROR"),TRUE,'index.php?action=control_panel&subtab=message');
-	else
-	    return $reply_data;
+        $reply_data=$this->_model->queryAll("SELECT * FROM reply WHERE pid=$mid");
+        if($reply_data)
+            $reply_data=$reply_data[0];
+        return $reply_data;
+	    #ZFramework::show_message(ZFramework::t("QUERY_ERROR"),TRUE,'index.php?action=control_panel&subtab=message');
+
     }
-    public  function actionDelete()
-    {
+    public  function actionDelete(){
         is_admin();
         $mid=isset($_GET['mid'])?(int)$_GET['mid']:null;
         if($mid!==null){
-	    $condition=array('id'=>$mid);
-	    if(!$this->_model->delete(REPLYTABLE, $condition))
-		die($this->_model->error());
+            $this->_model->query("DELETE FROM reply WHERE pid=$mid");
         }
         header("Location:index.php?action=control_panel&subtab=message&randomvalue=".rand());
     }
-    public  function actionDeleteAll()
-    {
+    public  function actionDeleteAll(){
         is_admin();
-        $this->_model->truncate(DB, REPLYTABLE);
+        $this->_model->query("DELETE FROM reply");
         header("location:index.php?action=control_panel&subtab=message");
     }
-    
 }
--- trunk/themes/simple/templates/reply.php
+++ trunk/themes/simple/templates/reply.php
@@ -9,16 +9,10 @@
 <body>
     <form action="index.php?controller=reply&amp;action=reply" method="post">
 <input type="hidden" name="mid" value="<?php echo $mid;?>" />
-<?php 
-if($reply_data)
-{
-    $reply_data=$reply_data[0];
-?>
+<?php if($reply_data){ ?>
 <input type="hidden" name="update" value="update" />
-<?php
-}
-?>
-<textarea name="reply_content" cols="40" rows="9"><?php echo @str_replace('<br />', "\n", $reply_data['reply_content']);?></textarea>
+<?php } ?>
+<textarea name="content" cols="40" rows="9"><?php echo @str_replace('<br />', "\n", $reply_data['content']);?></textarea>
 <br />
 <input type="submit" name="Submit" value="<?php echo ZFramework::t('SUBMIT');?>" />
 <a href="index.php?action=control_panel&amp;subtab=message"><?php echo ZFramework::t('CANCEL');?></a>
