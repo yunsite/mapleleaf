750124bc2e0b1b4ef7c702d5a83513a0d1017738
dreamneverfall@gmail.com <dreamneverfall@gmail.com@b4eaa4ea-357a-11de-8b57-c5b9ca66747e>
2011-04-03 02:59:43 +0000

    增加 index.php 避免目录被浏览

--- trunk/includes/ZFramework.php
+++ trunk/includes/ZFramework.php
@@ -36,6 +36,10 @@ class ZFramework{
         'UserController/actionUpdate',
         );
 
+	/**
+	 * 返回一个本类的实例
+	 *
+	 */
     public static function createApp(){
         if(!(self::$_instance instanceof  self)){
             self::$_instance=new self();
@@ -56,6 +60,10 @@ class ZFramework{
             return null;
     }
 
+	/**
+	 * 构造函数
+	 *
+	 */
     private function  __construct(){
         global $db_url;
         $this->preloadAllControllers();
@@ -70,6 +78,11 @@ class ZFramework{
             $this->_params[$key]=$value;
         }
     }
+	
+	/**
+	 * 预载入所有 controller
+	 *
+	 */
     protected function preloadAllControllers(){
         $dir=dirname(__FILE__).'/'.$this->_controllerPath;
         $d=dir($dir);
@@ -81,10 +94,20 @@ class ZFramework{
         $d->close();
     }
 
+	/**
+	 * 执行IP过滤
+	 *
+	 */
     protected function performIPFilter(){
         if(is_baned(getIP()))
             die('Access denied!');
     }
+
+	/**
+	 * 判断是否已安装
+	 *
+	 * @return bool
+	 */
     protected function is_installed(){
         global $db_url;
         
@@ -95,11 +118,21 @@ class ZFramework{
         }
         return true;
     }
+
+	/**
+	 * 判断是否是关闭模式
+	 *
+	 */
     protected function is_closedMode(){
         $disabledAction=array('PostController/actionCreate','SiteController/actionIndex','UserController/actionCreate');
         if($this->site_close==1 && !isset ($_SESSION['admin']) && in_array($this->_controller.'/'.$this->_action, $disabledAction))
             self::show_message($this->close_reason);
     }
+
+	/**
+	 * 执行任务
+	 *
+	 */
     public function run(){
         global $API_CODE;
         #var_dump($this->_controller.'/'.$this->_action);exit;
@@ -142,16 +175,40 @@ class ZFramework{
         }
     }
 
+	/**
+	 * 得到 GET 参数
+	 *
+	 * @return array
+	 */
     public function getParams(){
         return $this->_params;
     }
+	
+	/**
+	 * 返回当前的 controller
+	 *
+	 * @return string
+	 */
     public function getController(){
         return $this->_controller;
     }
+
+	/**
+	 * 返回当前的 action
+	 * @return string
+	 */
     public function getAction(){
         return $this->_action;
     }
 
+	/**
+	 * 翻译指定信息
+	 *
+ 	 * @param mixed $message
+	 * @param array $params
+	 * @param mixed $userSpecifiedLanguage
+	 * @return string
+	 */
     public static function t($message,$params=array(),$userSpecifiedLanguage=null){
         $messages=self::getLangArray($userSpecifiedLanguage);
         if(isset ($messages[$message]) && $messages[$message]!=='')
@@ -159,6 +216,12 @@ class ZFramework{
         return $params!==array()?strtr($message, $params):$message;
     }
     
+	/**
+	 * 得到指定语言的语言翻译信息
+	 *
+	 * @param mixed $userSpecifiedLanguage
+	 * @return array
+	 */
     public static function getLangArray($userSpecifiedLanguage=null){
         if($userSpecifiedLanguage){
             if(file_exists(APPROOT.'/languages/'.$userSpecifiedLanguage.'.php')){
@@ -167,19 +230,31 @@ class ZFramework{
         }
         return include APPROOT.'/languages/'.self::createApp()->lang.'.php';
     }
+
+	/**
+	 * 得到表情图案
+	 *
+	 */
     public static function getSmileys(){
         return include  dirname(__FILE__).'/smiley.php';
     }
+
+	/**
+	 * 转义字符串
+	 *
+	 */
     public static function maple_quotes($var,$charset='UTF-8'){
         return htmlspecialchars(trim($var),ENT_QUOTES,  $charset);
     }
+
     /**
      * 显示信息
      */
     public static  function show_message($msg,$redirect=false,$redirect_url='index.php',$time_delay=3){
         include 'themes/'.self::createApp()->theme.'/templates/'."show_message.php"; exit;
     }
-        /**
+
+    /**
      * 得到所有可用的主题
      */
     public static  function get_all_themes(){
@@ -190,10 +265,14 @@ class ZFramework{
                 $themes[$entry]=$entry;
         }
         $d->close();
-        return $themes;
+        return array_filter($themes,array(__CLASS__,'_removeIndex'));
     }
-
-
+	
+	/**
+	 * 得到所有可用的语言。在 languages/ 目录里搜寻语言文件
+	 *
+	 * @return array
+	 */
     public static  function get_all_langs(){
     	$langs=array();
         $d=dir(APPROOT.'/languages/');
@@ -202,10 +281,20 @@ class ZFramework{
                 $langs[substr($entry,0,-4)]=substr($entry,0,-4);
         }
         $d->close();
-        return $langs;
+        return array_filter($langs,array(__CLASS__,'_removeIndex'));
     }
+
+	/**
+	 * 得到所有的时区，翻译后的时区信息
+	 *
+	 * @return array
+	 */
     public static  function get_all_timezone(){
         $timezone=  include APPROOT.'/languages/'.self::app()->lang.'.php';
     	return $timezone['TZ_ZONES'];
     }    
+
+	protected function _removeIndex($var){
+		return (!($var == 'index' || $var == 'index.php'));
+	}
 }
